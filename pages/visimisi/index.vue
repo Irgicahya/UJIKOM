cf00e799604b7a72a9ec1ef99a59721f97696824
Irgi Cahya Setiawan <142205032+Irgicahya@users.noreply.github.com>
2024-12-17 11:25:55 +0700

    Add files via upload

---
+++ pages/visimisi/index.vue
@@ -0,0 +1,22 @@
+<template> 
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <header class="header mt-3"> <!-- Menambahkan margin-top pada header -->
+      <img src="/assets/image/ultah.jpeg" alt="Header Background" class="header-image mb-3" />
+      <div class="header-overlay"></div>
+    </header>
+
+        <h4 style="background-color: black;" class="text-white text-center rounded-1 p-2 m-4">
+          <b>VISI - MISI - TUJUAN </b>
+        </h4>
+      </div>
+    </div>
+
+    <p style="background-color: honeydew;" class="text-black text-left rounded-1 p-2 m-3">
+      <NuxtLink to="/visimisi/" class="text-decoration-none">Profil / Visi & Misi </NuxtLink>
+    </p>
+
+    
+  </div>
+</template>
